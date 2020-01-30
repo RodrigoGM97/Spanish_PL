85d73409410519743f96f18ed4b9004967c02891
RodrigoGM97 <rodrigogm971@gmail.com>
2020-01-29 22:00:34 -0600

    added arrays

--- spanishPL.py
+++ spanishPL.py
@@ -2,7 +2,7 @@ tokens = (
     'NAME','INT', 'FLOAT', 'STRING', 'NUM', 'TEXTO',
     'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
     'LPAREN','RPAREN','FOR', 'SI', 'LBRACE', 'RBRACE',
-    'EOC'
+    'EOC', 'LBRACKET', 'RBRACKET', 'APPEND'
     )
 
 # Tokens
@@ -16,8 +16,10 @@ t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_LBRACE  = r'\{'
 t_RBRACE  = r'\}'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
 t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
-t_STRING  = r'"[a-zA-Z0-9_ ]*"'
+t_STRING  = r'\"[a-zA-Z0-9_ ]*\"'
 t_EOC     = r'\;'
 
 def t_FLOAT(t): #we put float above integer because otherwise it will check if it is an int first and crash on floats.
@@ -66,6 +68,14 @@ def t_TEXTO(t):
         print("ERR")
     return t
 
+def t_APPEND(t):
+    r'.anadefin'
+    try:
+        print("Entro append sintaxis")
+    except ValueError:
+        print("ERR")
+    return t
+
 # Ignored characters
 t_ignore = " \t"
 
@@ -96,6 +106,7 @@ variables = { }
 def p_statement_expr(t):
     '''statement : empty
                  | assign
+                 | arrayOP
                  | expression EOC statement'''
     
     #print(t)
@@ -106,10 +117,23 @@ def p_statement_empty(t):
 
 def p_statement_assign(t):
     '''assign : NUM NAME EQUALS expression EOC statement
-              | TEXTO NAME EQUALS expression EOC statement'''
+              | TEXTO NAME EQUALS string_expression EOC statement'''
     #print("Enter assign")
     print("Asigna: "+str(t[4]))
     variables[t[2]] = t[4]
+
+def p_statement_assign_array(t):
+    '''assign : NUM LBRACKET RBRACKET NAME EOC statement
+              | TEXTO LBRACKET RBRACKET NAME EOC statement'''
+
+    variables[t[4]] = []
+
+def p_arrayOP_append(t):
+    '''arrayOP : NUM NAME APPEND LPAREN expression RPAREN EOC statement
+               | TEXTO NAME APPEND LPAREN string_expression RPAREN EOC statement'''
+
+    print("append: "+str(t[5]))
+    variables[t[2]].append(t[5])
 
 def p_expression_binop(t):
     '''expression : expression PLUS expression
@@ -120,6 +144,11 @@ def p_expression_binop(t):
     elif t[2] == '-': t[0] = t[1] - t[3]
     elif t[2] == '*': t[0] = t[1] * t[3]
     elif t[2] == '/': t[0] = t[1] / t[3]
+
+def p_string_expression_binop(t):
+    '''string_expression : string_expression PLUS string_expression'''
+    if t[2] == '+'  : t[0] = t[1] + t[3]
+
 
 def p_expression_uminus(t):
     'expression : MINUS expression %prec UMINUS'
@@ -138,13 +167,14 @@ def p_expression_group(t):
 
 def p_expression_number(t):
     '''expression : INT
-                  | FLOAT
-                  | STRING'''
+                  | FLOAT'''
     
+    t[0] = t[1]
+
+def p_string_expression_string(t):
+    '''string_expression : STRING'''   
     if (isinstance(t[1], str)):
         t[0] = t[1][1:-1]
-    else:
-        t[0] = t[1]
 
 
 def p_expression_name(t):
